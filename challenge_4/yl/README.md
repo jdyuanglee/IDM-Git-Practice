cbe897170faa2818a3b098180aee393a3f9940f7
jdyuanglee <yuanglee39@gmail.com>
2018-07-30 15:46:45 -0400

    :tada: Challenge 4 by Yuang Li

---
+++ challenge_4/yl/README.md
@@ -0,0 +1,16 @@
+# Stylus Code Editor
+
+## Installation
+1. Download or clone the file from Github repo
+2. Make sure you have Node.js and gulp installed
+3. Open the terminal and go to the flolder you just downloaded
+4. Install node_modules by typing command `npm install`
+
+## Command
+* Run the code by typing command `gulp`
+* A tab will pops out or a exisited tab get refreshed
+* Edit your stylus code directly from the file and hit save
+* The browser will refreshes the page automatically
+* Hit `Ctrl + C` to exit the editing mode
+
+[View on GitHub](https://github.com/philsinatra/IDM-Git-Practice/tree/master/challenge_4/yl)
---
+++ challenge_4/yl/gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+gulp.task('css', () => {
+  return gulp.src('src/stylus/screen.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', () => {
+  return gulp.src('src/views/index.html')
+    // .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('serve', ['css'], () =>{
+  browserSync.init({
+    server: 'dist/'
+  });
+
+  gulp.watch('src/stylus/screen.styl',['css']);
+  gulp.watch('src/views/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['css','html','serve']);
